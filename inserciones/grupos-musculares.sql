82faa786ea8614c05e9d458363f3a83994c18646
Jordy Jaimes <jaimesjorgejordy@gmail.com>
2025-08-26 11:28:08 -0500

    Se insertó la información de los grupos musculares, en total fueron 8, una breve descripción y el bloque funcional al que pertenecen

---
+++ inserciones/grupos-musculares.sql
@@ -0,0 +1,17 @@
+INSERT INTO grupos_musculares (nombre_grupo, descripcion_grupo, bloque_funcional) 
+VALUES 
+('Brazos', 'Incluye bíceps, tríceps y antebrazos. Los bíceps flexionan el codo, los tríceps lo extienden y los antebrazos controlan muñeca y agarre. Tren superior', 'tren superior'),
+
+('Pecho', 'Formado por pectoral mayor, menor y serrato. Situado en el torso frontal, permite empujes como press de banca o flexiones. Tren superior', 'tren superior'),
+
+('Espalda', 'Compuesta por dorsal ancho, trapecio, romboides y erectores. Ubicada en la parte posterior, realiza tracción y extensión de columna', 'tren superior'),
+
+('Hombros', 'Incluye deltoides y manguito rotador. Permiten abducción, flexión y extensión del brazo en ejercicios como press militar', 'tren superior'),
+
+('Abdomen', 'Formado por recto abdominal, oblicuos y transverso. Estabiliza el tronco y permite flexión y rotación como en crunches o giros', 'core / abdomen'),
+
+('Piernas', 'Incluye cuádriceps, isquiotibiales y aductores. Permiten extensión y flexión de rodilla y cadera en sentadillas y peso muerto', 'tren inferior'),
+
+('Glúteos', 'Formados por glúteo mayor, medio y menor. Extienden y abducen la pierna, claves en hip thrusts, sentadillas y peso muerto', 'tren inferior'),
+
+('Gemelos', 'Compuestos por gastrocnemio y sóleo. Realizan flexión plantar del tobillo en movimientos como elevaciones de talones', 'tren inferior');
