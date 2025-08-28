8d230e4b9e5676b0f7ace3af1ccaad7cd7195541
Jordy Jaimes <jaimesjorgejordy@gmail.com>
2025-08-27 20:19:09 -0500

    Se modificó la tabla de ejercicios, se eliminó el atributo de instrucciones que no se veía necesario ya que se cambió por un nuevo atributo que es una imagen o gif del ejercicio en cuestión, así la instrucción es visual y no en texto

--- modificaciones-consultas/modificaciones.sql
+++ modificaciones-consultas/modificaciones.sql
@@ -1,2 +1,21 @@
 ALTER TABLE grupos_musculares
 ADD COLUMN bloque_funcional VARCHAR(50) NOT NULL CONSTRAINT ck_grupo_muscular CHECK (LOWER(bloque_funcional) IN ('tren superior', 'tren inferior', 'core / abdomen'));
+
+ALTER TABLE ejercicios
+DROP COLUMN instrucciones,
+ALTER COLUMN equipo_necesario TYPE VARCHAR(50),
+ADD COLUMN url_imagen VARCHAR(300),
+ADD CONSTRAINT ck_equipo_necesario CHECK (
+    LOWER(equipo_necesario) IN (
+        'ninguno',
+        'mancuerna',
+        'barra',
+        'banda elastica',
+        'silla',
+        'banco',
+        'kettlebell',
+        'maquina',
+        'cuerda',
+        'otros'
+    )
+);
