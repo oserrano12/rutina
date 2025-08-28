550518e60b4bf0bd1542763bbed983dd58db6713
Jordy Jaimes <jaimesjorgejordy@gmail.com>
2025-08-27 20:37:31 -0500

    Se añadieron 5 ejercicios de flexiones con su respectiva información, se requiere modificar las url porque no son precisas

---
+++ inserciones/ingreso-ejercicios.sql
@@ -0,0 +1,8 @@
+SELECT * FROM ejercicios;
+
+INSERT INTO ejercicios (nombre_ejercicio, descripcion_ejercicio, tipo_ejercicio, equipo_necesario, dificultad, url_imagen) 
+VALUES ('Flexiones de Brazo', 'Ejercicio de peso corporal que trabaja el pecho, los hombros y los tríceps.', 'casa', 'ninguno', 'principiante', 'https://hips.hearstapps.com/hmg-prod/images/pushup-1525365829.gif?resize=980:*'),
+('Flexiones Diamante', 'Ejercicio de peso corporal que enfatiza los tríceps y el pecho interno.', 'casa', 'ninguno', 'intermedio', 'https://lyftaweb.s3.us-east-2.amazonaws.com/GymvisualMP4/37851202.mp4'),
+('Flexiones con Rodillas', 'Es uno de los tipos de flexiones para trabajar el pecho más sencillas. Ideal para aquellos amateurs', 'casa', 'ninguno', 'principiante', 'https://lyftaweb.s3.us-east-2.amazonaws.com/GymvisualMP4/37851201.mp4'),
+('Flexiones de Hombros', 'Ejercicio avanzado que trabaja los hombros y el core.', 'casa', 'ninguno', 'avanzado', 'https://i.pinimg.com/originals/e7/67/f1/e767f1775ecefbc1ee5366162727da59.gif'),
+('Flexiones Inclinadas', 'Este tipo de flexión consiste en utilizar una superficie como una silla para realizar las flexiones', 'casa', 'silla', 'intermedio', 'https://lyftaweb.s3.us-east-2.amazonaws.com/GymvisualMP4/37851201.mp4');
